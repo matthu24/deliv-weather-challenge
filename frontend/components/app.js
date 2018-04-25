20c4d6c367fabf302ae3cf23171363e920e7a6f4
Matthew Hu <matthu24@gmail.com>
2018-04-24 17:32:18 -0700

    Add footer

--- frontend/components/app.js
+++ frontend/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import WeatherList from './weather_list/weather_list_container';
 import Search from './search/search_container';
 import Header from './header/header';
+import Footer from './footer/footer';
 
 export default class App extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class App extends Component {
         <Header/>
         <Search/>
         <WeatherList/>
+        <Footer/>
       </div>
     );
   }
